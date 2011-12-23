2d1b7fabcc12a450fb9a2916729bc9235fb58b3b
Nathan Osman <nathan.osman@gmail.com>
2011-12-23 12:56:02 -0800

    Began moving JavaScript code to separate .js file and added background page.

---
+++ stackapi.js
@@ -0,0 +1,15 @@
+//==========================
+// Provides JavaScript code
+// with access to the Stack
+//      Exchange API.
+//==========================
+
+var StackAPI = {
+    
+    MakeRequest: function(url) {
+        
+        //...
+        
+    }
+    
+};
