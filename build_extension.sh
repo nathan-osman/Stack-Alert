c823a4bacb21692fee6915d859ab0c7d4fea9165
Nathan Osman <nathan.osman@gmail.com>
2012-01-07 22:19:45 -0800

    Updated build script to work with Google Chrome.

--- build_extension.sh
+++ build_extension.sh
@@ -4,21 +4,42 @@
 #      Chrome
 #====================
 
+echo -e "\033[34mPackaging Chrome extension...\033[m"
+
 # Switch to the directory and copy the JS / CSS files
 cp common/stackalert.js chrome
 cp common/style.css chrome
 
-# Generate the CRX, using an existing key if present
-if [ -f stackalert.pem ] ; then KEY="--pack-extension-key=stackalert.pem" ; fi
-chromium-browser --pack-extension=chrome $KEY
+# Check for an existing PEM key
+KEYFILE="stackalert.pem"
+
+if [ -f $KEYFILE ] ; then
+    KEYARG="--pack-extension-key=$KEYFILE"
+    echo -e "\033[32m - found existing key: $KEYFILE\033[m"
+fi
+
+ARGS="--pack-extension=chrome $KEYARG"
+
+# Check to see if Chrome or Chromium is installed
+if ! chromium-browser $ARGS 2>/dev/null ; then
+    if ! google-chrome $ARGS 2>/dev/null ; then
+        echo -e "\033[1m\033[31mError: neither Google Chrome nor Chromium was found on your system. At least one of them is required to run this script.\033[m"
+        exit 1
+    fi
+fi
 
 # Clean up
 mv chrome.crx stackalert.crx
-mv chrome.pem stackalert.pem
+
+if [ -f chrome.pem ] ; then
+    mv chrome.pem stackalert.pem
+fi
 
 #====================
 #      Firefox
 #====================
+
+echo -e "\033[34mPackaging Firefox add-on...\033[m"
 
 # Switch to the directory and copy the JS / CSS files
 cd firefox
@@ -27,3 +48,5 @@ cp ../common/style.css chrome/skin/popup.css
 
 # Now create the add-on
 zip -r ../stackalert.xpi .
+
+echo -e "\033[32mPackaging complete!\033[m"
