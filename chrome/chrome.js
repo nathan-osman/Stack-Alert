bc99ed967695c5a5a396539e71a24add66acb91b
Nathan Osman <nathan.osman@gmail.com>
2012-01-09 22:02:48 -0800

    Fixed missing icon size assignment in Chrome extension.

--- chrome/chrome.js
+++ chrome/chrome.js
@@ -1,5 +1,6 @@
 // This file sets the browser and API key for Chrome
 
 StackAlert.Browser  = 'chrome';
+StackAlert.IconSize = '19';
 StackAlert.APIKey   = 'jo7ss)kjUytEaOELX5xdcg((';
 StackAlert.ClientID = '24';
