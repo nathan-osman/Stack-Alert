da27ced180adbbef1c782e98cb70dc9cfc0326b7
Nathan Osman <nathan.osman@gmail.com>
2013-03-18 16:24:18 -0700

    Update stackalert.js
    Updated year to 2013.

--- common/stackalert.js
+++ common/stackalert.js
@@ -1,5 +1,5 @@
 // Stack Alert - Monitor your Stack Exchange inbox.
-// Copyright (C) 2012 Nathan Osman
+// Copyright (C) 2013 Nathan Osman
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
