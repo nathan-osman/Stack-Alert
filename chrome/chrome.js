e5dfc2d748fdd82aca0f2415a10e9ca41e1756bb
Nathan Osman <nathan@quickmediasolutions.com>
2016-04-30 19:37:54 -0700

    Fixed Chrome build.

--- chrome/chrome.js
+++ chrome/chrome.js
@@ -4,3 +4,20 @@ StackAlert.Browser  = 'chrome';
 StackAlert.IconSize = '19';
 StackAlert.APIKey   = 'jo7ss)kjUytEaOELX5xdcg((';
 StackAlert.ClientID = '24';
+
+// Perform the initial update
+StackAlert.PerformUpdate();
+
+// Listen for requests to update the data
+chrome.extension.onRequest.addListener(function(request, sender) {
+    
+    // The command to perform is listed in request
+    if(request.command == 'update')
+        StackAlert.PerformUpdate();
+    else if(request.command == 'authorize') {
+        StackAlert.CompleteAuthorization(request.hash);
+        chrome.tabs.remove(sender.tab.id);
+        StackAlert.PerformUpdate();
+    }
+    
+});
---
+++ chrome/popup.js
@@ -0,0 +1,4 @@
+var div = document.createElement('div');
+StackAlert.GeneratePopupHTML(window, div);
+
+document.getElementsByTagName('body')[0].appendChild(div);
